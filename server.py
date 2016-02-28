f29cd949e6634a053686a33d04fcfc8c1ca9b6fa
Pamela San <pas2172@columbia.edu>
2016-02-28 16:15:11 -0500

    first commit

---
+++ server.py
@@ -0,0 +1,44 @@
+from autobahn.twisted.websocket import WebSocketServerProtocol, \
+    WebSocketServerFactory
+
+
+class MyServerProtocol(WebSocketServerProtocol):
+
+    def onConnect(self, request):
+	print("from header Origin is "  + request.headers.get('origin'))
+	print "origin is is %s" % request.headers.get('origin')
+#	print("from header Authorization is " + request.headers.get('Authorization'))	
+	print("from header authorization is " + request.headers.get('authorization'))
+        print("Client connecting: {0}".format(request.peer))
+
+    def onOpen(self):
+        print("WebSocket connection open.")
+
+    def onMessage(self, payload, isBinary):
+        if isBinary:
+            print("Binary message received: {0} bytes".format(len(payload)))
+        else:
+            print("Text message received: {0}".format(payload.decode('utf8')))
+
+        # echo back message verbatim
+        self.sendMessage(payload, isBinary)
+
+    def onClose(self, wasClean, code, reason):
+        print("WebSocket connection closed: {0}".format(reason))
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    from twisted.python import log
+    from twisted.internet import reactor
+
+    log.startLogging(sys.stdout)
+
+    factory = WebSocketServerFactory(u"ws://127.0.0.1:9000", debug=False)
+    factory.protocol = MyServerProtocol
+    # factory.setProtocolOptions(maxConnections=2)
+
+    reactor.listenTCP(9000, factory)
+    reactor.run()
